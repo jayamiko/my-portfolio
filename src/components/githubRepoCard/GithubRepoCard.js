ff233f3a7890ecc43863a0efeb8c2015ec443590
jayamiko <jayamiko4@gmail.com>
2025-03-07 15:02:35 +0700

    add project and publication data

--- src/components/githubRepoCard/GithubRepoCard.js
+++ src/components/githubRepoCard/GithubRepoCard.js
@@ -2,6 +2,7 @@ import React from "react";
 import ProjectLanguages from "../../components/projectLanguages/ProjectLanguages";
 import "./GithubRepoCard.css";
 import { Fade } from "react-reveal";
+import moment from "moment";
 
 export default function GithubRepoCard({ repo, theme }) {
   function openRepoinNewTab(url) {
@@ -10,7 +11,7 @@ export default function GithubRepoCard({ repo, theme }) {
   }
 
   return (
-    <div className="repo-card-div" style={{ backgroundColor: theme.highlight }}>
+    <div className="repo-card-div" style={{ backgroundColor: "#95C1E6" }}>
       <Fade bottom duration={2000} distance="40px">
         <div key={repo.id} onClick={() => openRepoinNewTab(repo.url)}>
           <div className="repo-name-div">
@@ -39,7 +40,8 @@ export default function GithubRepoCard({ repo, theme }) {
               className="repo-creation-date subTitle"
               style={{ color: theme.secondaryText }}
             >
-              Created on {repo.createdAt.split("T")[0]}
+              {/* Created on {repo.createdAt.split("T")[0]} */}
+              Created on {moment(repo.createdAt.split("T")[0]).format("LL")}
             </p>
             <ProjectLanguages
               className="repo-languages"
@@ -47,31 +49,55 @@ export default function GithubRepoCard({ repo, theme }) {
             />
           </div>
           {/* <div className="repo-stats">
-          <div className="repo-left-stat">
-            <span>
-              <div className="language-color" style={{ backgroundColor: repo.node.primaryLanguage.color }}></div>
-              <p>{repo.node.primaryLanguage.name}</p>
-            </span>
-            <span>
-              <svg aria-hidden="true" className="octicon" height="16" role="img" viewBox="0 0 10 16" width="10" fill="rgb(106, 115, 125)" className="repo-star-svg">
-                <path
-                  fill-rule="evenodd"
-                  d="M8 1a1.993 1.993 0 0 0-1 3.72V6L5 8 3 6V4.72A1.993 1.993 0 0 0 2 1a1.993 1.993 0 0 0-1 3.72V6.5l3 3v1.78A1.993 1.993 0 0 0 5 15a1.993 1.993 0 0 0 1-3.72V9.5l3-3V4.72A1.993 1.993 0 0 0 8 1zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3 10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3-10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"
-                ></path>
-              </svg>
-              <p>{repo.node.forkCount}</p>
-            </span>
-            <span>
-              <svg aria-hidden="true" className="octicon" height="16" role="img" viewBox="0 0 14 16" width="14" fill="rgb(106, 115, 125)" className="repo-star-svg">
-                <path fill-rule="evenodd" d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z"></path>
-              </svg>
-              <p>{repo.node.stargazers.totalCount}</p>
-            </span>
-          </div>
-          <div className="repo-right-stat">
-            <p>{repo.node.diskUsage} KB</p>
-          </div>
-        </div> */}
+            <div className="repo-left-stat">
+              <span>
+                <div
+                  className="language-color"
+                  style={{ backgroundColor: repo.node.primaryLanguage.color }}
+                ></div>
+                <p>{repo.node.primaryLanguage.name}</p>
+              </span>
+              <span>
+                <svg
+                  aria-hidden="true"
+                  className="octicon"
+                  height="16"
+                  role="img"
+                  viewBox="0 0 10 16"
+                  width="10"
+                  fill="rgb(106, 115, 125)"
+                  // className="repo-star-svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M8 1a1.993 1.993 0 0 0-1 3.72V6L5 8 3 6V4.72A1.993 1.993 0 0 0 2 1a1.993 1.993 0 0 0-1 3.72V6.5l3 3v1.78A1.993 1.993 0 0 0 5 15a1.993 1.993 0 0 0 1-3.72V9.5l3-3V4.72A1.993 1.993 0 0 0 8 1zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3 10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3-10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"
+                  ></path>
+                </svg>
+                <p>{repo.node.forkCount}</p>
+              </span>
+              <span>
+                <svg
+                  aria-hidden="true"
+                  className="octicon"
+                  height="16"
+                  role="img"
+                  viewBox="0 0 14 16"
+                  width="14"
+                  fill="rgb(106, 115, 125)"
+                  // className="repo-star-svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z"
+                  ></path>
+                </svg>
+                <p>{repo.node.stargazers.totalCount}</p>
+              </span>
+            </div>
+            <div className="repo-right-stat">
+              <p>{repo.node.diskUsage} KB</p>
+            </div>
+          </div> */}
         </div>
       </Fade>
     </div>
--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import GithubRepoCard from "../../components/githubRepoCard/GithubRepoCard";
@@ -16,25 +16,108 @@ import ProjectsData from "../../shared/opensource/projects.json";
 import "./Projects.css";
 import ProjectsImg from "./ProjectsImg";
 
-class Projects extends Component {
-  render() {
-    const theme = this.props.theme;
-    return (
-      <div className="projects-main">
-        <Header theme={theme} />
+const Projects = ({ theme, onToggle }) => {
+  const [repos, setRepos] = useState([]);
+  const username = "jayamiko"; // Ganti dengan username Anda
+
+  useEffect(() => {
+    const fetchRepos = async () => {
+      try {
+        const response = await fetch(
+          `https://api.github.com/users/${username}/repos`
+        );
+        const data = await response.json();
+
+        // Format data agar sesuai dengan struktur yang diinginkan
+        const formattedData = data.map((repo) => ({
+          id: repo.id,
+          name: repo.name,
+          createdAt: repo.created_at,
+          url: repo.html_url,
+          description: repo.description,
+          isFork: repo.fork,
+          languages: [], // Bahasa akan diambil dalam langkah terpisah
+        }));
+
+        setRepos(formattedData);
+      } catch (error) {
+        console.error("Error fetching repositories:", error);
+      }
+    };
+
+    fetchRepos();
+  }, []);
+
+  return (
+    <div className="projects-main">
+      <Header theme={theme} />
+
+      {/* Projects Section */}
+      <div className="basic-projects">
+        <Fade bottom duration={2000} distance="40px">
+          <div className="projects-heading-div">
+            <div className="projects-heading-img-div">
+              <ProjectsImg theme={theme} />
+            </div>
+            <div className="projects-heading-text-div">
+              <h1
+                className="projects-heading-text"
+                style={{ color: theme.text }}
+              >
+                Project & Publication
+              </h1>
+              <p
+                className="projects-header-detail-text subTitle"
+                style={{ color: theme.secondaryText }}
+              >
+                I develop innovative projects using the latest technologies and
+                contribute to research through publications, focusing on
+                impactful solutions and advancements in tech.
+              </p>
+            </div>
+          </div>
+        </Fade>
+      </div>
+
+      {/* Publications Section */}
+      {publications.data.length > 0 && (
         <div className="basic-projects">
           <Fade bottom duration={2000} distance="40px">
-            <div className="projects-heading-div">
-              <div className="projects-heading-img-div">
-                {/* <img
-											src={require(`../../assets/images/${projectsHeader["avatar_image_path"]}`)}
-											alt=""
-										/> */}
-                <ProjectsImg theme={theme} />
-              </div>
-              <div className="projects-heading-text-div">
+            <div className="publications-heading-div">
+              <div className="publications-heading-text-div">
                 <h1
-                  className="projects-heading-text"
+                  className="publications-heading-text"
+                  style={{ color: theme.text }}
+                >
+                  {publicationsHeader.title}
+                </h1>
+                <p
+                  className="projects-header-detail-text subTitle"
+                  style={{ color: theme.secondaryText }}
+                >
+                  {publicationsHeader.description}
+                </p>
+              </div>
+            </div>
+          </Fade>
+        </div>
+      )}
+
+      {/* Publications Cards */}
+      <div className="repo-cards-div-main">
+        {publications.data.map((pub) => (
+          <PublicationCard key={pub.id} pub={pub} theme={theme} />
+        ))}
+      </div>
+
+      {/* Publications Section */}
+      {repos?.length > 0 && (
+        <div className="basic-projects">
+          <Fade bottom duration={2000} distance="40px">
+            <div className="publications-heading-div">
+              <div className="publications-heading-text-div">
+                <h1
+                  className="publications-heading-text"
                   style={{ color: theme.text }}
                 >
                   {projectsHeader.title}
@@ -43,60 +126,34 @@ class Projects extends Component {
                   className="projects-header-detail-text subTitle"
                   style={{ color: theme.secondaryText }}
                 >
-                  {projectsHeader["description"]}
+                  {projectsHeader.description}
                 </p>
               </div>
             </div>
           </Fade>
         </div>
-        <div className="repo-cards-div-main">
-          {ProjectsData.data.map((repo) => {
-            return <GithubRepoCard repo={repo} theme={theme} />;
-          })}
-        </div>
-        <Button
-          text={"More Projects"}
-          className="project-button"
-          href={greeting.githubProfile}
-          newTab={true}
-          theme={theme}
-        />
+      )}
 
-        {/* Publications  */}
-        {publications.data.length > 0 ? (
-          <div className="basic-projects">
-            <Fade bottom duration={2000} distance="40px">
-              <div className="publications-heading-div">
-                <div className="publications-heading-text-div">
-                  <h1
-                    className="publications-heading-text"
-                    style={{ color: theme.text }}
-                  >
-                    {publicationsHeader.title}
-                  </h1>
-                  <p
-                    className="projects-header-detail-text subTitle"
-                    style={{ color: theme.secondaryText }}
-                  >
-                    {publicationsHeader["description"]}
-                  </p>
-                </div>
-              </div>
-            </Fade>
-          </div>
-        ) : null}
-
-        <div className="repo-cards-div-main">
-          {publications.data.map((pub) => {
-            return <PublicationCard pub={pub} theme={theme} />;
-          })}
-        </div>
-
-        <Footer theme={this.props.theme} onToggle={this.props.onToggle} />
-        <TopButton theme={this.props.theme} />
+      {/* GitHub Repo Cards */}
+      <div className="repo-cards-div-main">
+        {repos.map((repo) => (
+          <GithubRepoCard key={repo.id} repo={repo} theme={theme} />
+        ))}
       </div>
-    );
-  }
-}
+
+      {/* More Projects Button */}
+      <Button
+        text={"More Projects"}
+        className="project-button"
+        href={greeting.githubProfile}
+        newTab={true}
+        theme={theme}
+      />
+
+      <Footer theme={theme} onToggle={onToggle} />
+      <TopButton theme={theme} />
+    </div>
+  );
+};
 
 export default Projects;
